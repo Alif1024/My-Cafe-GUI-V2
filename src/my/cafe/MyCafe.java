8a8cf611b7d70ec168f366556df283f60740500f
Alif1024 <nuraliffff1024@gmail.com>
2022-06-01 22:46:37 +0700

    MyCafe

---
+++ src/my/cafe/MyCafe.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package my.cafe;
+
+/**
+ *
+ * @author nural
+ */
+public class MyCafe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 1; i>2;i++)
+        {
+        System.out.println(i);
+        }
+        /*
+        login newlogin = new login();
+        newlogin.setVisible(true);*/
+    }
+    
+}
